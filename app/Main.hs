a5a808aec8a3b5f9e543e4ed4fa99f3179f12a93
Enigmage <alizaidi0786.az@gmail.com>
2023-02-06 13:04:35 +0530

    argument parser single file conversion complete

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,42 @@
 module Main (main) where
 
 import Lib qualified
+import OptParse
+import System.Directory (doesFileExist)
+import System.Exit (exitFailure)
+import System.FilePath.Posix (takeBaseName)
+import System.IO (IOMode (..), hClose, openFile, stdin, stdout)
 
 main :: IO ()
-main = Lib.main
+main =
+  parse >>= \case
+    ConvertDir input output -> putStrLn "not implemented yet"
+    ConvertSingle input output -> do
+      (title, inputHandler) <-
+        case input of
+          Stdin ->
+            pure ("", stdin)
+          InputFile file ->
+            (,) (takeBaseName file) <$> openFile file ReadMode
+      outputHandler <-
+        case output of
+          Stdout -> pure stdout
+          OutputFile file -> do
+            exists <- doesFileExist file
+            shouldOpenFile <- if exists then confirm else pure True
+            if shouldOpenFile
+              then openFile file WriteMode
+              else exitFailure
+
+      Lib.processSingle title inputHandler outputHandler
+      hClose inputHandler
+      hClose outputHandler
+
+confirm :: IO Bool
+confirm =
+  putStrLn "File already exists. Overwrite? y/n"
+    *> getLine
+    >>= \case
+      "y" -> pure True
+      "n" -> pure False
+      _ -> putStrLn "Confirm by writing y or n" *> confirm
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,46 +1,16 @@
-module Lib (main, processFileContent) where
+module Lib (processSingle, processDir) where
 
-import Control.Monad (when)
 import Lib.Convert (markupToHtml)
 import Lib.Html (Title, render)
 import Lib.Markup (parse)
-import System.Directory (doesFileExist)
-import System.Environment (getArgs)
-import System.FilePath.Posix (takeBaseName)
 import System.IO (Handle, hGetContents, hPutStrLn)
 
 processFileContent :: Title -> String -> String
 processFileContent title = render . markupToHtml title . parse
 
 processSingle :: Title -> Handle -> Handle -> IO ()
-processSingle title inp out = do
-  content <- hGetContents inp
-  hPutStrLn out (processFileContent title content)
+processSingle title inp out =
+  hGetContents inp >>= hPutStrLn out . processFileContent title
 
 processDir :: FilePath -> FilePath -> IO ()
 processDir = undefined
-
-main :: IO ()
-main =
-  getArgs
-    >>= \case
-      [inputPath, outputPath] -> do
-        fileContent <- readFile inputPath
-        exists <- doesFileExist outputPath
-        let writeResult = writeFile outputPath (processFileContent (takeBaseName outputPath) fileContent)
-        if not exists then writeResult else whenIO confirm writeResult
-      _ -> putStrLn "Usage: ssghs <input file path> <output file path>"
-
--- * > chain IO without passing value
-
-whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = cond >>= \res -> when res action
-
-confirm :: IO Bool
-confirm =
-  putStrLn "File already exists. Overwrite? y/n"
-    *> getLine
-    >>= \case
-      "y" -> pure True
-      "n" -> pure False
-      _ -> putStrLn "Confirm by writing y or n" *> confirm
